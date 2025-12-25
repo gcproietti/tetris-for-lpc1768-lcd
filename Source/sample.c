76f98adb8929f103c19a8126acd48112895aaae4
Giacomo <giacomo.proietti1@studenti.unipg.it>
2025-12-25 18:10:46 +0100

    Added update_score
    Added update_score funct and relative uint16_to_ascii_uint8 funct and tetrisInit

--- Source/sample.c
+++ Source/sample.c
@@ -48,6 +48,12 @@ typedef	struct
 
 void LCD_tetraminoes(uint8_t shape, Coord_str xy, uint8_t set);		//if set is equal to 1 it draw the tetraminoes otherwise it erase it
 void tetrisInit();
+void update_score(uint16_t score, uint16_t topScore, uint16_t lines);
+void uint16_to_ascii_uint8(uint16_t val, uint8_t *dest);
+
+uint16_t score = 0;
+uint16_t topScore = 0;
+uint16_t lines = 0;
 
 
 int main(void)
@@ -75,7 +81,7 @@ int main(void)
 	coord.x = 100;
 	coord.y = 250;
 	
-	LCD_tetraminoes(Z_Shaped, coord, 1);
+	LCD_tetraminoes(S_Shaped, coord, 1);
 	
 	
 	
@@ -109,13 +115,87 @@ void tetrisInit(){
 	}
 	//----END: DRAW PLAYING FIELD----
 	
+	update_score(0,0,0);
+
+}
+
+/******************************************************************************
+* Function Name  : update_score
+* Description    : this function update the written value of the current score, highest score and deleted lines on the display
+* Input          : - score: value of score
+*                  - topScore: value of highest score
+*									 - lines: value of deleted lines
+* Output         : None
+* Return         : None
+* Attention		   : None
+*******************************************************************************/
+void update_score(uint16_t score, uint16_t topScore, uint16_t lines){
+	
+	uint8_t score_char_ptr[6];
+	uint8_t topScore_char_ptr[6];
+	uint8_t lines_char_ptr[6];
+	
+	uint16_to_ascii_uint8(score, score_char_ptr);
+	uint16_to_ascii_uint8(topScore, topScore_char_ptr);
+	uint16_to_ascii_uint8(lines, lines_char_ptr);
+	
 	GUI_Text(160, 40, (uint8_t *) "SCORE", Red, Black);
-	GUI_Text(160, 120, (uint8_t *) "TOP SCORE", Red, Black);
-	GUI_Text(160, 200, (uint8_t *) "LINES", Red, Black);
+	GUI_Text(160, 55, (uint8_t *) score_char_ptr, White, Black);
+	
+	GUI_Text(160, 100, (uint8_t *) "TOP SCORE", Red, Black);
+	GUI_Text(160, 115, (uint8_t *) topScore_char_ptr, White, Black);
+
+	GUI_Text(160, 160, (uint8_t *) "LINES", Red, Black);
+	GUI_Text(160, 175, (uint8_t *) lines_char_ptr, White, Black);
 
 }
 
 
+/**
+ * Converts a uint16_t to ASCII in a uint8_t buffer.
+ * Max value 65535 requires a 6-byte buffer.
+ */
+void uint16_to_ascii_uint8(uint16_t val, uint8_t *dest) {
+    // We work from the end of the potential max string length
+    // [d1][d2][d3][d4][d5][\0]
+    //  0   1   2   3   4   5  <- Index
+    
+    uint8_t buffer[5]; 
+    int8_t i = 4;
+
+    if (val == 0) {
+        dest[0] = '0';
+        dest[1] = '\0';
+        return;
+    }
+
+    // Decompose number into digits from right to left
+    while (val > 0 && i >= 0) {
+        buffer[i--] = (uint8_t)(val % 10) + '0';
+        val /= 10;
+    }
+
+    // 'i' now points to the index before the first digit
+    // We shift the result to the start of the dest pointer
+    uint8_t j = 0;
+		uint8_t k = 0;
+    for (k = i + 1; k <= 4; k++) {
+        dest[j++] = buffer[k];
+    }
+    
+    dest[j] = '\0'; // Null-terminate
+}
+
+/******************************************************************************
+* Function Name  : LCD_tetraminoes
+* Description    : this function draw/erase on the display the selected tetramino
+* Input          : - shape: selected ttraminoes
+*                  - xy: coordinates (top-left) of the tetramino
+*									 - set: if 1 it draw the tetramino, if 0 it erase the tetramino
+* Output         : None
+* Return         : None
+* Attention		   : None
+*******************************************************************************/
 void LCD_tetraminoes(uint8_t shape, Coord_str xy, uint8_t set){
 	
 	int i = 0;
@@ -184,6 +264,69 @@ void LCD_tetraminoes(uint8_t shape, Coord_str xy, uint8_t set){
 				}
 				break;
 			//----END: DRAW Z-SHAPED----
+				
+		case O_Shaped:
+			//----START: DRAW O-SHAPED----
+			for(l=0;l<(dim)+1;l = l+dim){
+				for(i=0;i<dim*2;i++){
+					LCD_DrawLine(x0+l, y0+i, (x0+dim)+l, y0+i, color);
+				}
+			}
+			//----END: DRAW O-SHAPED----
+		break;
+		case J_Shaped:
+			//----START: DRAW J-SHAPED----
+			for(l=0;l<(dim)+1;l = l+dim){
+				if(!l){
+				for(i=dim*2;i<(dim*3)+1;i++){
+					LCD_DrawLine(x0+l, y0+i, (x0+dim)+l, y0+i, color);
+				}}else{
+				
+				for(i=0;i<(dim*3)+1;i++){
+					LCD_DrawLine(x0+l, y0+i, (x0+dim)+l, y0+i, color);
+				}}
+				
+			}
+			//----END: DRAW J-SHAPED----
+		break;
+		case L_Shaped:
+			//----START: DRAW L-SHAPED----
+			for(l=0;l<(dim)+1;l = l+dim){
+				if(!l){
+					for(i=0;i<(dim*3)+1;i++){
+					LCD_DrawLine(x0+l, y0+i, (x0+dim)+l, y0+i, color);
+				}
+				}else{
+					for(i=dim*2;i<(dim*3)+1;i++){
+					LCD_DrawLine(x0+l, y0+i, (x0+dim)+l, y0+i, color);
+				
+				}}
+				
+			}
+			//----END: DRAW L-SHAPED----
+		break;
+		case S_Shaped:
+			//----START: DRAW S-SHAPED----
+				for(l=0;l<(dim*2)+1;l = l+dim){ //x-axe
+					if(!l){
+					for(i=dim;i<dim*2;i++){
+						LCD_DrawLine(x0+l, y0+i, (x0+dim)+l, y0+i, color);
+					}
+					}else if (l==dim){
+					
+					for(i=0;i<dim*2;i++){
+						LCD_DrawLine(x0+l, y0+i, (x0+dim)+l, y0+i, color);
+					}
+					}else if (l==2*dim){
+					for(i=0;i<dim;i++){
+						LCD_DrawLine(x0+l, y0+i, (x0+dim)+l, y0+i, color);
+					}
+					
+					}
+					
+				}
+			//----END: DRAW S-SHAPED----
+		break;
 		default:
         GUI_Text(x0, y0, (uint8_t *) "MISSING TETRAMINOES", Red, Black);
         break;
